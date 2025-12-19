c392404d7edc60053d1a1060f4c5711e40b8c785
Ben <ben.vd.helm@britge.io>
2025-12-19 17:19:10 +0100

    update

--- viewsets.py
+++ viewsets.py
@@ -11,6 +11,7 @@ class PriceGroupViewSet(TranslateMixin, ModelViewSetForm):
     serializer_class = PriceGroupSerializer
     field_name = 'price_group_objects'
     ordering_fields = ['created_time', 'modified_time']
+    admin_roles = ['Admin']
 
 class ProductPriceViewSet(TranslateMixin, ModelViewSetForm):
     queryset = ProductPrice.objects.all().order_by('-created_time').select_related('product', 'price_group')
@@ -18,6 +19,7 @@ class ProductPriceViewSet(TranslateMixin, ModelViewSetForm):
     field_name = 'product_price_objects'
     ordering_fields = ['created_time', 'modified_time']
     search_fields = ['product__translations__name', 'product__product_number', ]
+    admin_roles = ['Admin']
 
 class ProductDiscountGroupViewSet(TranslateMixin, ModelViewSetForm):
     queryset = ProductDiscountGroup.objects.all().prefetch_related( 
@@ -27,6 +29,7 @@ class ProductDiscountGroupViewSet(TranslateMixin, ModelViewSetForm):
             queryset=Discount.objects.all().select_related('customer')),
     )
     serializer_class = ProductDiscountGroupSerializer
+    admin_roles = ['Admin']
 
     @britge_action_detail( serializer_class=DiscountSerializer, serializer_fields = [ 'id', 'discount_abs', 'discount_perc', 'min_order_quantity', 'max_order_quantity', ] )
     def add_discount(self, request, pk=None ):
@@ -39,12 +42,15 @@ class ProductDiscountGroupViewSet(TranslateMixin, ModelViewSetForm):
 class CustomerDiscountGroupViewSet(TranslateMixin, ModelViewSetForm):
     queryset = CustomerDiscountGroup.objects.all()
     serializer_class = CustomerDiscountGroupSerializer
+    admin_roles = ['Admin']
     
 class DiscountViewSet( ModelViewSetForm):
     queryset = Discount.objects.all()
     serializer_class = DiscountSerializer
+    admin_roles = ['Admin']
              
 class DiscountCoupoonViewSet(TranslateMixin, ModelViewSetForm):
     queryset = DiscountCoupon.objects.all()
     serializer_class = DiscountCouponSerializer
 
+    admin_roles = ['Admin']
